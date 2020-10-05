532bbd009014e846cff357aacdd9e1af49378eb7
Giorgio Torre <giorgio.torre8@gmail.com>
2020-10-05 18:22:34 +0100

    Set up Education component styling

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -9,16 +9,16 @@ const Education = () => {
       "freeCodeCamp, Udemy, Code with Mosh, etc. to level up programming skills.",
   };
   const meetup = {
-    title: "Codecademy Meetup (London)",
+    title: "Codecademy Meetup",
     date: "Sept-2019 - Ongoing",
     description:
-      "Getting together with like minded people is the best way to keep motivation up. The host of the Codecademy Meetup in London helped me push my skills to the next level and I'm very grateful for that.",
+      "Tutoring, pair programming, problem solving and portfolio building.",
   };
   const pisa = {
     title: "Pisa University",
     date: "Sept-2010 - Aug-2014",
     description:
-      "BSc and MSc in Industrial and Management Engineering with related internships.",
+      "BSc / MSc in Industrial and Management Engineering with related internships.",
   };
   const cranfield = {
     title: "Cranfield University",
@@ -28,54 +28,43 @@ const Education = () => {
 
   return (
     <div className="content_wrapper">
-      <Card
-        title={online.title}
-        date={online.date}
-        description={online.description}
-      />
-      <Card
-        title={meetup.title}
-        date={meetup.date}
-        description={meetup.description}
-      />
-      <Card
-        title={pisa.title}
-        date={pisa.date}
-        description={pisa.description}
-      />
-      <Card
-        title={cranfield.title}
-        date={cranfield.date}
-        description={cranfield.description}
-      />
-      <div className="skills">
-        <h1>Skills:</h1>
-        <ul>
-          <li>
-            <i class="devicon-html5-plain-wordmark"></i>
-          </li>
-          <li>
-            <i class="devicon-css3-plain-wordmark"></i>
-          </li>
-          <li>
-            <i class="devicon-sass-original"></i>
-          </li>
-          <li>
-            <i class="devicon-javascript-plain"></i>
-          </li>
-          <li>
-            <i class="devicon-react-original-wordmark"></i>
-          </li>
-          <li>
-            <i class="devicon-git-plain-wordmark"></i>
-          </li>
-          <li>
-            <i class="devicon-github-plain-wordmark"></i>
-          </li>
-          <li>
-            <i class="devicon-heroku-plain-wordmark"></i>
-          </li>
-        </ul>
+      <h1 className="title">Education</h1>
+      <div className="card-wrapper">
+        <Card
+          title={online.title}
+          date={online.date}
+          description={online.description}
+        />
+        <Card
+          title={meetup.title}
+          date={meetup.date}
+          description={meetup.description}
+        />
+        <Card
+          title={pisa.title}
+          date={pisa.date}
+          description={pisa.description}
+        />
+        <Card
+          title={cranfield.title}
+          date={cranfield.date}
+          description={cranfield.description}
+        />{" "}
+      </div>
+      <div className="skills-wrapper">
+        <h1 className="title">Skills</h1>
+        <div className="skills">
+          <div className="skills__logo">
+            <i id="logo" class="devicon-html5-plain-wordmark"></i>
+            <i id="logo" class="devicon-css3-plain-wordmark"></i>
+            <i id="logo" class="devicon-sass-original"></i>
+            <i id="logo" class="devicon-javascript-plain"></i>
+            <i id="logo" class="devicon-react-original-wordmark"></i>
+            <i id="logo" class="devicon-git-plain-wordmark"></i>
+            <i id="logo" class="devicon-github-plain-wordmark"></i>
+            <i id="logo" class="devicon-heroku-plain-wordmark"></i>
+          </div>
+        </div>
       </div>
     </div>
   );

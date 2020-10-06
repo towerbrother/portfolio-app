e65e97c62bce37ff28e6ada7eeeac85c02f599c5
Giorgio Torre <giorgio.torre8@gmail.com>
2020-10-06 12:24:05 +0100

    Set up ToggleView component and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ToggleView from "./common/ToggleView";
 
 const Footer = () => (
   <div className="footer_wrapper">
     <h5 className="footer__text">
       <span>&#169;</span> 2020 Giorgio Torre. All rights reserved.
     </h5>
+    <ToggleView />
   </div>
 );
 

7b780bdeac8044ad97dc0c94a71f9876f8390fb6
Giorgio Torre <giorgio.torre8@gmail.com>
2020-10-07 15:36:09 +0100

    Defined color palette for light and dark mode, set up css variables and assigned throughout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../images/tower-logo.png";
+// import logo from "../images/tower-logo.png";
 
 const Header = () => (
   <div className="header_wrapper">
     <div className="header">
       <NavLink className="header__home" to="/">
-        <img className="header__home__logo" src={logo} alt="logo" />
+        {/* <img className="header__home__logo" src={logo} alt="logo" /> */}
         <div className="header__home__name">
           <h1 className="name">Tower</h1>
           <h5 className="role">Front End Developer</h5>
--- src/components/common/ToggleView.jsx
+++ src/components/common/ToggleView.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
-const ToggleView = () => (
-  <div className="toggle-view">
-    <input type="checkbox" className="toggle-view__checkbox" id="checkbox" />
-    <label className="toggle-view__label" for="checkbox">
-      <FontAwesomeIcon icon={faSun} className="toggle-view__icon" />
-      <FontAwesomeIcon icon={faMoon} className="toggle-view__icon" />
-      <div className="toggle-view__ball"></div>
-    </label>
-  </div>
-);
+const ToggleView = () => {
+  return (
+    <div className="toggle-view">
+      <input type="checkbox" className="toggle-view__checkbox" id="checkbox" />
+      <label className="toggle-view__label" htmlFor="checkbox">
+        <FontAwesomeIcon icon={faMoon} className="toggle-view__icon" />
+        <FontAwesomeIcon icon={faSun} className="toggle-view__icon" />
+        <div className="toggle-view__ball"></div>
+      </label>
+    </div>
+  );
+};
 
 export default ToggleView;

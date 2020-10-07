40a25fd09526ff6b6ccafd9c21cacfb4c6443cb1
Giorgio Torre <giorgio.torre8@gmail.com>
2020-10-07 15:50:01 +0100

    Dark theme logic implemented

--- src/components/common/ToggleView.jsx
+++ src/components/common/ToggleView.jsx
@@ -2,10 +2,26 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
+const darkTheme = "dark";
+
 const ToggleView = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    isDark
+      ? document.documentElement.classList.add(darkTheme)
+      : document.documentElement.classList.remove(darkTheme);
+  }, [isDark]);
+
   return (
     <div className="toggle-view">
-      <input type="checkbox" className="toggle-view__checkbox" id="checkbox" />
+      <input
+        type="checkbox"
+        className="toggle-view__checkbox"
+        id="checkbox"
+        checked={isDark}
+        onChange={(e) => setIsDark(e.target.checked)}
+      />
       <label className="toggle-view__label" htmlFor="checkbox">
         <FontAwesomeIcon icon={faMoon} className="toggle-view__icon" />
         <FontAwesomeIcon icon={faSun} className="toggle-view__icon" />

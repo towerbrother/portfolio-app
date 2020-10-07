4237a811a4584bc79e4602c5499fefc4d87880f2
Giorgio Torre <giorgio.torre8@gmail.com>
2020-10-07 17:11:59 +0100

    Set up Projects component.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 
-const Projects = () => (
-  <div className="content_wrapper">
-    <div className="projects">Projects</div>
-  </div>
-);
+const Projects = () => {
+  return (
+    <div className="content_wrapper">
+      <div className="projects-container">
+        <div className="project active" id="weather-app">
+          weather-app
+        </div>
+        <div className="project" id="indecision-app">
+          indecision-app
+        </div>
+        <div className="project" id="budget-app">
+          budget-app
+        </div>
+        <div className="project" id="video-app">
+          video-app
+        </div>
+        <div className="project" id="calculator">
+          calculator
+        </div>
+        <div className="project" id="clock">
+          clock
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;

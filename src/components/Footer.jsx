5d8c9b55f2781cd0bcdab0f78a66e73195fda651
Giorgio Torre <giorgio.torre8@gmail.com>
2020-10-09 20:05:46 +0100

    Removed text from Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,6 @@ import ToggleView from "./common/ToggleView";
 
 const Footer = () => (
   <div className="footer_wrapper">
-    <h5 className="footer__text">
-      <span>&#169;</span> 2020 Giorgio Torre. All rights reserved.
-    </h5>
     <ToggleView />
   </div>
 );

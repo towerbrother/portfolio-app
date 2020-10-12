a1608ab6370a730fc38cf0108435b230501e8926
Giorgio Torre <giorgio.torre8@gmail.com>
2020-10-12 22:39:40 +0100

    Updates

---
+++ precache-manifest.3adf6f216b0616dc34d5849a5f3efdd2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6b1aaffac8ec689d71c04b9831ade6",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "58467f3e8e0cf8b726bb",
+    "url": "/portfolio-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3e50ddae24feb5f25605",
+    "url": "/portfolio-app/static/css/main.b7ea0787.chunk.css"
+  },
+  {
+    "revision": "58467f3e8e0cf8b726bb",
+    "url": "/portfolio-app/static/js/2.81ad76c7.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/portfolio-app/static/js/2.81ad76c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e50ddae24feb5f25605",
+    "url": "/portfolio-app/static/js/main.601bc462.chunk.js"
+  },
+  {
+    "revision": "9273fb07e8f782d2211c",
+    "url": "/portfolio-app/static/js/runtime-main.9d41a791.js"
+  },
+  {
+    "revision": "223daec463cf1c2e146c64dc528fc67a",
+    "url": "/portfolio-app/static/media/budget-app.223daec4.PNG"
+  },
+  {
+    "revision": "bf8129391b5d982dffad71254e6929ec",
+    "url": "/portfolio-app/static/media/indecision-app.bf812939.PNG"
+  },
+  {
+    "revision": "050d7942fbbf8e5beef8d3773ec05c83",
+    "url": "/portfolio-app/static/media/video-app.050d7942.PNG"
+  },
+  {
+    "revision": "d20c3e7fdae6e7a81d5ebc4840af6c9b",
+    "url": "/portfolio-app/static/media/weather-app.d20c3e7f.PNG"
+  }
+]);

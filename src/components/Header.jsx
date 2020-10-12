2bfefc3325e5f36ffad719fb9fc6c7bafbb7b69a
Giorgio Torre <giorgio.torre8@gmail.com>
2020-10-12 16:08:08 +0100

    Adjusted mobile/tablet/desktop breakpoint values. Re-engineered Header component layout and style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,40 +2,57 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-import HeaderRoutes from "./HeaderRoutes";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="header_wrapper">
-      <div className="header">
-        <NavLink className="header__home" to="/">
-          <h1 className="name">Giorgio</h1>
-          <h1 className="surname">Torre</h1>
-        </NavLink>
-        <HeaderRoutes displaySize={"header__routes__desktop"} />
-        {isOpen ? (
-          <div>
-            <HeaderRoutes displaySize={"header__routes__mobile"} />
-            <a href onClick={() => setIsOpen(!isOpen)}>
-              <FontAwesomeIcon
-                icon={faTimes}
-                className="header__icon"
-                id="cross"
-              />
-            </a>
-          </div>
-        ) : (
-          <a href onClick={() => setIsOpen(!isOpen)}>
-            <FontAwesomeIcon
-              icon={faBars}
-              className="header__icon"
-              id="hamburger"
-            />
+      <ul className="header">
+        <li className="header__home">
+          <NavLink className="header__link" to="/">
+            <h1>
+              Giorgio <label className="is_colored">Torre</label>
+            </h1>
+          </NavLink>
+        </li>
+        <li className="header__routes__element">
+          <NavLink className="header__link" to="/about">
+            About
+          </NavLink>
+        </li>
+        <li className="header__routes__element">
+          <NavLink className="header__link" to="/skills">
+            Skills
+          </NavLink>
+        </li>
+        <li className="header__routes__element">
+          <NavLink className="header__link" to="/projects">
+            Projects
+          </NavLink>
+        </li>
+        <li className="header__routes__element">
+          <a
+            className="header__link"
+            href="mailto:giorgio.torre8@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contact
           </a>
-        )}
-      </div>
+        </li>
+        <li className="header__icon">
+          {isOpen ? (
+            <a href onClick={() => setIsOpen(!isOpen)}>
+              <FontAwesomeIcon icon={faTimes} />
+            </a>
+          ) : (
+            <a href onClick={() => setIsOpen(!isOpen)}>
+              <FontAwesomeIcon icon={faBars} />
+            </a>
+          )}
+        </li>
+      </ul>
     </div>
   );
 };

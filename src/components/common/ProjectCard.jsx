4e0d670f28c7c26d85d0d56ef21e228708e0f819
Giorgio Torre <giorgio.torre8@gmail.com>
2020-10-13 17:20:08 +0100

    Amended mobile and tablet styling for the ProjectCard component. Amended desktop-breakpoint to include iPad Pro landscape view.

--- src/components/common/ProjectCard.jsx
+++ src/components/common/ProjectCard.jsx
@@ -7,51 +7,94 @@ const ProjectCard = ({
   imgSrc,
   gitHubUrl,
   demoUrl,
-}) => (
-  <div className="project_card">
-    <img className="project_card__image" src={imgSrc} alt={title} />
-    <div className="project_card__overlay">
-      <div className="project_card__overlay__info">
-        <h1>{stack}</h1>
-        <h2>{title}</h2>
-        <h3>{description}</h3>
-        <div className="buttons">
-          {gitHubUrl === "" ? (
-            <button className="button__link" disabled>
-              Source
-            </button>
-          ) : (
-            <button>
-              <a
-                className="button__link"
-                href={gitHubUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+}) => {
+  return (
+    <div className="project-card-container">
+      <div className="project-card">
+        <img className="project-card__image" src={imgSrc} alt={title} />
+        <div className="project-card__overlay">
+          <div className="project-card__overlay__info">
+            <h1>{stack}</h1>
+            <h2>{title}</h2>
+            <h3>{description}</h3>
+            <div className="buttons">
+              {gitHubUrl === "" ? (
+                <button className="button__link" disabled>
+                  Source
+                </button>
+              ) : (
+                <button>
+                  <a
+                    className="button__link"
+                    href={gitHubUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Source
+                  </a>
+                </button>
+              )}
+              {demoUrl === "" ? (
+                <button className="button__link" disabled>
+                  Demo
+                </button>
+              ) : (
+                <button>
+                  <a
+                    className="button__link"
+                    href={demoUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Demo
+                  </a>
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="project-card__mobile">
+        <div className="project-card__mobile__info">
+          <div className="mobile_title">{title}</div>
+          <div className="buttons">
+            {gitHubUrl === "" ? (
+              <button className="button__link" disabled>
                 Source
-              </a>
-            </button>
-          )}
-          {demoUrl === "" ? (
-            <button className="button__link" disabled>
-              Demo
-            </button>
-          ) : (
-            <button>
-              <a
-                className="button__link"
-                href={demoUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              </button>
+            ) : (
+              <button>
+                <a
+                  className="button__link"
+                  href={gitHubUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Source
+                </a>
+              </button>
+            )}
+            {demoUrl === "" ? (
+              <button className="button__link" disabled>
                 Demo
-              </a>
-            </button>
-          )}
+              </button>
+            ) : (
+              <button>
+                <a
+                  className="button__link"
+                  href={demoUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Demo
+                </a>
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ProjectCard;

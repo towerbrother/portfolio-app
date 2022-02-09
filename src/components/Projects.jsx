b3584da5d806a6a4cbb7f571d7f2046632553abb
GTO <giorgio.torre@stellenanzeigen.de>
2022-02-09 08:12:42 +0100

    added new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,12 +3,20 @@ import ProjectCard from "./common/ProjectCard";
 import weatherAppImg from "../images/weather-app.PNG";
 import expensesAppImg from "../images/expenses-app.PNG";
 import indecisionAppImg from "../images/indecision-app.PNG";
-// import videoAppImg from "../images/video-app.PNG";
+import musicAppImg from "../images/music-app.PNG";
 import calculatorImg from "../images/calculator.PNG";
 import clockImg from "../images/clock.PNG";
 import drumkitImg from "../images/drumkit.PNG";
 
 const Projects = () => {
+  const musicApp = {
+    title: "Music App",
+    stack: "React",
+    description: "Nothing better than relaxing music while coding.",
+    gitHubUrl: "https://github.com/towerbrother/waves-music-app",
+    demoUrl: "https://towerbrother.github.io/waves-music-app",
+  };
+
   const weatherApp = {
     title: "Weather App",
     stack: "React",
@@ -32,14 +40,6 @@ const Projects = () => {
     gitHubUrl: "https://github.com/towerbrother/expenses-app",
     demoUrl: "https://towerbrother-expenses-app.herokuapp.com/",
   };
-
-  // const videoApp = {
-  //   title: "Video App",
-  //   stack: "Firestore/React",
-  //   description: "Coming soon...",
-  //   gitHubUrl: "",
-  //   demoUrl: "",
-  // };
 
   const calculator = {
     title: "JS Calculator",
@@ -65,17 +65,17 @@ const Projects = () => {
     demoUrl: "https://towerbrother.github.io/JS-drumkit/",
   };
 
-  // const snake = {
-  //   title: "JS Snake",
-  //   stack: "Javascript",
-  //   description: "The good old days...",
-  //   gitHubUrl: "https://github.com/towerbrother/JS-snake",
-  //   demoUrl: "https://towerbrother.github.io/JS-snake/",
-  // };
-
   return (
     <div className="content_wrapper">
       <div className="projects-container">
+        <ProjectCard
+          title={musicApp.title}
+          stack={musicApp.stack}
+          description={musicApp.description}
+          imgSrc={musicAppImg}
+          gitHubUrl={musicApp.gitHubUrl}
+          demoUrl={musicApp.demoUrl}
+        />
         <ProjectCard
           title={weatherApp.title}
           stack={weatherApp.stack}
